25e35ec9e6194917033463987f4b0847395714a9
Alex DS <alex0360@outlook.com>
2022-02-22 23:47:12 -0400

    [IMP] global object 'sqlConnection'

--- DemoDatabase/Data/ClientRepository.cs
+++ DemoDatabase/Data/ClientRepository.cs
@@ -6,21 +6,17 @@ namespace DemoDatabase.Data
 {
     public class ClientRepository
     {
-        private string SqlConnectionString { get; set; }
         private readonly string UMLAUT = "¨"; //Hex = 00A8; //Dec = 0168; //Entidad = &uml; //Carácter = diéresis
+
+        private SqlConnection sqlConnection = null;
 
         public void LoginInSqlServer(string dataSource, string dataBase, string user, string password)
         {
-            string conString = $"Data Source={dataSource}; Initial Catalog={dataBase}; User Id ={user}; Password={password}";
-
-            SqlConnection con = null;
             try
             {
-                con = new SqlConnection(conString);
+                sqlConnection = new SqlConnection($"Data Source={dataSource}; Initial Catalog={dataBase}; User Id ={user}; Password={password}");
 
-                con.Open();
-
-                SqlConnectionString = conString;
+                sqlConnection.Open();
             }
             catch(SqlException exception)
             {
@@ -28,30 +24,26 @@ namespace DemoDatabase.Data
             }
             finally
             {
-                if(con.State == ConnectionState.Open)
+                if(sqlConnection.State == ConnectionState.Open)
                 {
-                    con.Close();
+                    sqlConnection.Close();
                 }
             }
         }
 
         public string[] GetAll(string sqlCommand)
         {
-            string[] result = { " " };
+            string[] resultOfQuery = { " " };
 
-            Array.Resize(ref result, 1);
+            Array.Resize(ref resultOfQuery, 1);
 
-            if(SqlConnectionString == string.Empty)
+            if(sqlConnection.ConnectionString == string.Empty)
             {
                 throw new Exception("No ha hecho login");
             }
 
-            var sqlConnection = new SqlConnection();
-
             try
             {
-                sqlConnection.ConnectionString = SqlConnectionString;
-
                 sqlConnection.Open();
 
                 var cmd = new SqlCommand(sqlCommand, sqlConnection);
@@ -67,16 +59,16 @@ namespace DemoDatabase.Data
                     {
                         temporaryRecord = temporaryRecord + records.GetValue(i) + UMLAUT;
                     }
-                    result[result.Length - 1] = temporaryRecord;
+                    resultOfQuery[resultOfQuery.Length - 1] = temporaryRecord;
 
-                    Array.Resize(ref result, result.Length + 1);
+                    Array.Resize(ref resultOfQuery, resultOfQuery.Length + 1);
                 }
 
                 records.Close();
 
-                Array.Resize(ref result, result.Length - 1);
+                Array.Resize(ref resultOfQuery, resultOfQuery.Length - 1);
 
-                return result;
+                return resultOfQuery;
             }
             catch(Exception ex)
             {
