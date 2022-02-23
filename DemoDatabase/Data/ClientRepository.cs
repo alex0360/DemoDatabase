33540e8c2e469644794b66c1896cf84f4b9c175e
Alex DS <alex0360@outlook.com>
2022-02-22 23:35:28 -0400

    [IMP] delete comment

--- DemoDatabase/Data/ClientRepository.cs
+++ DemoDatabase/Data/ClientRepository.cs
@@ -39,51 +39,41 @@ namespace DemoDatabase.Data
         {
             string[] result = { " " };
 
-            //AQUI DEBEMOS COLOCAR LA INSTRUCCION PARA DIMENSIONAR EL ARREGLO EN CERO (0)
             Array.Resize(ref result, 1);
 
-            //VALIDO A VER SI EL STRING CONNECTION (SC) TIENE DATOS
             if(SqlConnectionString == string.Empty)
             {
                 throw new Exception("No ha hecho login");
             }
 
-            //EN CASO DE TENER.. DEFINO LA VARIABLE CON LA QUE ME VOY A COMUNICAR CON LA BD
             var sqlConnection = new SqlConnection();
 
             try
             {
-                //ASIGNO EL SC A LA VARIABLE DE CONEXIÓN
                 sqlConnection.ConnectionString = SqlConnectionString;
-                //APERTURO LA CONEXIÓN
+
                 sqlConnection.Open();
-                //SI NO HUBO ERROR.. DEFINO LA VARIABLE CON LA QUE EJECUTARÉ EL COMANDO REIBIDO COMO PARAMETRO
+
                 var cmd = new SqlCommand(sqlCommand, sqlConnection);
-                //EJECUTO LA INSTRUCCIÓN Y EL RESULTADO LO GUARDO EN LA VARIABLE "REGISTRO"
+
                 var records = cmd.ExecuteReader();
 
-                //HAGO UN RECORRIDO REGISTRO X REGISTRO DE LA VARIABLE "REGISTRO"
                 while(records.Read())
                 {
-                    //LIMPIAMOS LA VARIABLES QUE ACUMULARA LOS CAMPOS DEL REGISTRO
+
                     var temporaryRecord = string.Empty;
 
-                    //POR CADA CAMPO QUE TENGA EL REGISTRO QUE SE ESTA LEYENDO EN ESTE PASE
                     for(int i = 0; i < records.FieldCount; i++)
                     {
-                        //ACUMULO EN LA VARIABLE "strRegistro" EL VALOR DEL CAMPO EN LA POSICION "i" DEL REGISTRO
                         temporaryRecord = temporaryRecord + records.GetValue(i) + UMLAUT;
                     }
-                    //AQUI GUARDO EN LA MAYOR POSICIÓN DEL ARREGLO (INICIALMENTE ES 0) EL CONTENIDO DE LA VARIABLE: strRegistro
                     result[result.Length - 1] = temporaryRecord;
 
-                    //ACA INCREMENTO EN 1 EL TRAMAÑO DEL ARREGLO, PERO!!!!! "PRESERVANDO EL CONTENIDO" QUE HASTA ESE MOMENTO HAYA ACUMULADO
                     Array.Resize(ref result, result.Length + 1);
                 }
 
                 records.Close();
 
-                //ACA DISMINUYO EN 1 EL TRAMAÑO DEL ARREGLO, YA QUE LA ULT. POSICIÓN ESTA EN BLANCO
                 Array.Resize(ref result, result.Length - 1);
 
                 return result;
